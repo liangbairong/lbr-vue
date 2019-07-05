7a85c0414439285e2c3f7f9b723b685ca502cbe0
梁栢荣 <bairong.liang@symbio.com>
2019-07-05 15:36:34 +0800

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,28 +1,27 @@
 <template>
   <div class="home">
-    <lbr-button class="red">fd</lbr-button>
-
+    <lbr-load-move>
+      <lbr-button class="red">fd</lbr-button>
+    </lbr-load-move>
   </div>
 </template>
 
 <script>
-  import {lbrButton} from '@/packages/index'
 export default {
-  name: 'home',
-  components: {
-   'lbr-button': lbrButton
-    // HelloWorld
-  },
-  created(){
-    console.log('home')
+  name: "home",
+  created() {
+    console.log("home");
+    this.$toast({
+      message: "提示",
+      position: "bottom",
+      duration: 5000
+    });
   }
-}
+};
 </script>
 <style lang="scss" scoped>
-
-.logo{
-  width:750px;
+.logo {
+  width: 750px;
 }
-
 </style>
 
