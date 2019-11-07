0c6cfe7726156932cef8824dc16a2c2aa97aef83
lbr <903529487@qq.com>
2019-11-07 11:39:23 +0800

    1

--- gulpfile.js
+++ gulpfile.js
@@ -10,11 +10,11 @@ const del = require('del');
 // })
 
 function cops(){
-  return gulp.src('./docs/.vuepress/dist/**/*').pipe(gulp.dest('./docs_dist'))
+  return gulp.src('./docs_md/.vuepress/dist/**/*').pipe(gulp.dest('./docs'))
 }
 function dels(){
   return del([
-    './docs/.vuepress/dist/',
+    './docs_md/.vuepress/dist/',
   ]);
 }
 gulp.task('default', gulp.series(cops, dels));
